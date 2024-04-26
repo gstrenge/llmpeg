8ce9f2d3dbe88e3cd9f329cf7fcdb49ea0a33971
gstrenge <gstrenge01@gmail.com>
2024-04-25 20:28:20 -0400

    Increment patch version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="llmpeg",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
